02ccc9f95dd5c650fdbc92224d36e4a184129d89
ElementalCodeHub <155996715+ElementalCodeHub@users.noreply.github.com>
2024-02-16 20:01:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
-// Use the path module to ensure correct file paths
-import path from 'path';
-
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   build: {
-    // Set the base path to your repository name
-    base: '/Pomodoro-vue/',
     // Configure CSS extraction
     cssCodeSplit: true,
   },
